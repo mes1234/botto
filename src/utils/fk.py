cc7bd6354983c8ed2105d6a904ffdee34c4ac720
mes1234 <witold.kepczynski@gmail.com>
2025-10-21 21:48:39 +0200

    wip

---
+++ src/utils/fk.py
@@ -0,0 +1,53 @@
+from typing import List
+import numpy as np
+from scipy.spatial.transform import Rotation as R
+
+
+class Position:
+    def __init__(
+        self, x: float, y: float, z: float, x_1: float, y_1: float, z_1: float
+    ):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.x_1 = x_1
+        self.y_1 = y_1
+        self.z_1 = z_1
+
+
+class FK_Leg:
+    def __init__(self, l1: float, l2: float):
+        self.l1 = l1
+        self.l2 = l2
+        pass
+
+    def compute_fk(self, alfa_1: float, alfa_2: float, alfa_3: float) -> Position:
+        # Existing rotations
+        rot_alfa_1 = R.from_rotvec(np.array([1.0, 0.0, 0.0]) * alfa_1)
+        rot_alfa_2 = R.from_rotvec(np.array([0.0, 1.0, 0.0]) * alfa_2)
+
+        # Initial points
+        point_mid = np.array([0.0, 0.0, -self.l1])
+        point_end = np.array([0.0, 0.0, -self.l1 - self.l2])
+
+        # Apply the first two rotations
+        rot12 = rot_alfa_1 * rot_alfa_2
+        fk_mid = rot12.apply(point_mid)
+        fk_end_pre_rot3 = rot12.apply(point_end)
+
+        # Rotation around y-axis by alfa_3 **around point_mid**
+        local_y = rot12.apply([0.0, 1.0, 0.0])
+        rot_alfa_3 = R.from_rotvec(local_y * alfa_3)
+
+        # Translate fk_end_pre_rot3 to origin relative to fk_mid
+        relative = fk_end_pre_rot3 - fk_mid
+
+        # Rotate relative vector
+        rotated_relative = rot_alfa_3.apply(relative)
+
+        # Translate back
+        fk_end = fk_mid + rotated_relative
+
+        return Position(
+            fk_end[0], fk_end[1], fk_end[2], fk_mid[0], fk_mid[1], fk_mid[2]
+        )
---
+++ tests/fk_test.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pytest
+from src.utils.fk import FK_Leg
+from pytest import approx
+
+
+def test_init_position():
+    leg = FK_Leg(1.0, 1.0)
+    pos = leg.compute_fk(0.0, 0.0, 0.0)
+    assert pos.x == approx(0.0)
+    assert pos.y == approx(0.0)
+    assert pos.z == approx(-2.0)
+
+
+@pytest.mark.parametrize(
+    "alfa_1, alfa_2, alfa_3, x,y,z",
+    [
+        (0.0, 0.0, 0.0, 0.0, 0.0, -2.0),  # neutral position
+        # alfa_2 rotations
+        (0.0, -np.pi / 2.0, 0.0, 2.0, 0.0, 0.0),
+        (0.0, np.pi / 2.0, 0.0, -2.0, 0.0, 0.0),
+        # alfa_3 rotations
+        (0.0, 0.0, -np.pi / 2.0, 1.0, 0.0, -1.0),
+        (0.0, 0.0, np.pi / 2.0, -1.0, 0.0, -1.0),
+        (0.0, np.pi / 2.0, np.pi / 2.0, -1.0, 0.0, 1.0),
+        # alfa_1 rotations
+        (np.pi / 2.0, 0.0, 0.0, 0.0, 2.0, 0.0),
+        (np.pi / 2.0, -np.pi / 2.0, 0.0, 2.0, 0.0, 0.0),
+        (np.pi / 2.0, 0.0, -np.pi / 2.0, 1.0, 1.0, 0.0),
+        (np.pi / 2.0, -np.pi / 2.0, -np.pi / 2.0, 1.0, -1.0, 0.0),
+        # random
+        (0.0, np.pi / 2.0, np.pi / 2.0, -1.0, 0.0, 1.0),
+        (np.pi / 2.0, np.pi / 2.0, np.pi / 2.0, -1.0, -1.0, 0.0),
+    ],
+)
+def test_fk(alfa_1: float, alfa_2: float, alfa_3: float, x: float, y: float, z: float):
+    leg = FK_Leg(1.0, 1.0)
+    pos = leg.compute_fk(alfa_1, alfa_2, alfa_3)
+    actual_x = float(pos.x)
+    actual_y = float(pos.y)
+    actual_z = float(pos.z)
+    assert actual_x == approx(x)
+    assert actual_y == approx(y)
+    assert actual_z == approx(z)
